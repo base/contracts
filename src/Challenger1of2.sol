b3de7559a80b205954caed6c1b353e5811411797
Michael de Hoog <michael.dehoog@coinbase.com>
2025-04-12 12:21:43 +1000

    Fix a bunch of spelling errors

--- src/Challenger1of2.sol
+++ src/Challenger1of2.sol
@@ -22,7 +22,7 @@ contract Challenger1of2 {
     address public immutable OP_SIGNER;
 
     /**
-     * @dev The address of counter party's signer (likely a multisig)
+     * @dev The address of counterparty's signer (likely a multisig)
      */
     address public immutable OTHER_SIGNER;
 
@@ -48,7 +48,7 @@ contract Challenger1of2 {
     /**
      * @dev Constructor to set the values of the constants.
      * @param _opSigner Address of Optimism signer.
-     * @param _otherSigner Address of counter party signer.
+     * @param _otherSigner Address of counterparty signer.
      * @param _l2OutputOracleProxy Address of the L2OutputOracleProxy contract.
      */
     constructor(address _opSigner, address _otherSigner, address _l2OutputOracleProxy) {
@@ -66,7 +66,7 @@ contract Challenger1of2 {
     //////////////////////////////////////////////////////////////*/
     /**
      * @dev Executes a call as the Challenger (must be called by
-     * Optimism or counter party signer).
+     * Optimism or counterparty signer).
      * @param _data Data for function call.
      */
     function execute(bytes memory _data) external {
