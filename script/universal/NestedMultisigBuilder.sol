a064cdbd87d1994436dd093e2a30a24666755b89
Michael de Hoog <michael.dehoog@coinbase.com>
2025-04-20 21:52:52 +1000

    Reintroduce nested multisig contracts for backwards compatibility (#137)

---
+++ script/universal/NestedMultisigBuilder.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.15;
+
+import {MultisigScript} from "./MultisigScript.sol";
+
+/**
+ * @title NestedMultisigBuilder
+ * @custom:deprecated Use `MultisigScript` instead.
+ */
+abstract contract NestedMultisigBuilder is MultisigScript {
+    /*
+     * @custom:deprecated Use `sign(address[] memory _safes)` instead.
+     */
+    function sign(address _signerSafe) public {
+        sign(_toArray(_signerSafe));
+    }
+
+    /*
+     * @custom:deprecated Use `verify(address[] memory _safes, bytes memory _signatures)` instead.
+     */
+    function verify(address _signerSafe, bytes memory _signatures) public view {
+        verify(_toArray(_signerSafe), _signatures);
+    }
+
+    /*
+     * @custom:deprecated Use `approve(address[] memory _safes, bytes memory _signatures)` instead.
+     */
+    function approve(address _signerSafe, bytes memory _signatures) public {
+        approve(_toArray(_signerSafe), _signatures);
+    }
+
+    /*
+     * @custom:deprecated Use `simulate(bytes memory _signatures)` instead, with empty `_signatures`.
+     */
+    function simulate() public {
+        simulate("");
+    }
+
+    /*
+     * @custom:deprecated Use `run(bytes memory _signatures)` instead, with empty `_signatures`.
+     */
+    function run() public {
+        run("");
+    }
+}
