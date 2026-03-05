1f6f3a174c65c4f1cc62aa9d4da07e0ec8590251
Leopold Joy <leo@leopoldjoy.com>
2026-03-05 01:12:53 +0000

    Fix stack-too-deep in DeployImplementations and regenerate semver-lock

--- scripts/deploy/DeployImplementations.s.sol
+++ scripts/deploy/DeployImplementations.s.sol
@@ -719,12 +719,15 @@ contract DeployImplementations is Script {
     function deployAggregateVerifierImpl(Input memory _input, Output memory _output) private {
         address zkVerifier = address(new MockVerifier());
 
-        SystemConfigGlobal scgImpl = new SystemConfigGlobal(INitroEnclaveVerifier(_input.nitroEnclaveVerifier));
-        vm.label(address(scgImpl), "SystemConfigGlobalImpl");
-        _output.systemConfigGlobalImpl = scgImpl;
-        address teeVerifierImpl = address(new TEEVerifier(scgImpl));
+        address teeVerifierImpl;
+        {
+            SystemConfigGlobal scgImpl = new SystemConfigGlobal(INitroEnclaveVerifier(_input.nitroEnclaveVerifier));
+            vm.label(address(scgImpl), "SystemConfigGlobalImpl");
+            _output.systemConfigGlobalImpl = scgImpl;
+            teeVerifierImpl = address(new TEEVerifier(scgImpl));
+        }
 
-        IVerifier aggregateVerifierImpl = IVerifier(
+        _output.aggregateVerifierImpl = IVerifier(
             address(
                 new AggregateVerifier(
                     GameType.wrap(uint32(_input.multiproofGameType)),
@@ -742,8 +745,7 @@ contract DeployImplementations is Script {
                 )
             )
         );
-        vm.label(address(aggregateVerifierImpl), "AggregateVerifierImpl");
-        _output.aggregateVerifierImpl = aggregateVerifierImpl;
+        vm.label(address(_output.aggregateVerifierImpl), "AggregateVerifierImpl");
     }
 
     function assertValidInput(Input memory _input) private pure {
