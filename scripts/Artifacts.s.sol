093312df8aaa91ac50d5ca40705bf40a8173fe52
Jack Chuma <jack.chuma@coinbase.com>
2026-05-12 10:28:51 -0400

    clean up Artifacts.s.sol

--- scripts/Artifacts.s.sol
+++ scripts/Artifacts.s.sol
@@ -19,6 +19,7 @@ contract Artifacts {
     error InvalidDeployment(string);
 
     mapping(string => address payable) internal _namedDeployments;
+    mapping(bytes32 => address payable) internal _predeploys;
     string internal deploymentOutfile;
 
     function setUp() public virtual {
@@ -28,6 +29,25 @@ contract Artifacts {
 
         uint256 chainId = Config.chainID();
         console.log("Connected to network with chainid %s", chainId);
+
+        _predeploys[keccak256("L2CrossDomainMessenger")] = payable(Predeploys.L2_CROSS_DOMAIN_MESSENGER);
+        _predeploys[keccak256("L2ToL1MessagePasser")] = payable(Predeploys.L2_TO_L1_MESSAGE_PASSER);
+        _predeploys[keccak256("L2StandardBridge")] = payable(Predeploys.L2_STANDARD_BRIDGE);
+        _predeploys[keccak256("L2ERC721Bridge")] = payable(Predeploys.L2_ERC721_BRIDGE);
+        _predeploys[keccak256("SequencerFeeVault")] = payable(Predeploys.SEQUENCER_FEE_WALLET);
+        _predeploys[keccak256("OptimismMintableERC20Factory")] = payable(Predeploys.OPTIMISM_MINTABLE_ERC20_FACTORY);
+        _predeploys[keccak256("OptimismMintableERC721Factory")] = payable(Predeploys.OPTIMISM_MINTABLE_ERC721_FACTORY);
+        _predeploys[keccak256("L1Block")] = payable(Predeploys.L1_BLOCK_ATTRIBUTES);
+        _predeploys[keccak256("GasPriceOracle")] = payable(Predeploys.GAS_PRICE_ORACLE);
+        _predeploys[keccak256("L1MessageSender")] = payable(Predeploys.L1_MESSAGE_SENDER);
+        _predeploys[keccak256("WETH")] = payable(Predeploys.WETH);
+        _predeploys[keccak256("LegacyERC20ETH")] = payable(Predeploys.LEGACY_ERC20_ETH);
+        _predeploys[keccak256("ProxyAdmin")] = payable(Predeploys.PROXY_ADMIN);
+        _predeploys[keccak256("BaseFeeVault")] = payable(Predeploys.BASE_FEE_VAULT);
+        _predeploys[keccak256("L1FeeVault")] = payable(Predeploys.L1_FEE_VAULT);
+        _predeploys[keccak256("OperatorFeeVault")] = payable(Predeploys.OPERATOR_FEE_VAULT);
+        _predeploys[keccak256("SchemaRegistry")] = payable(Predeploys.SCHEMA_REGISTRY);
+        _predeploys[keccak256("EAS")] = payable(Predeploys.EAS);
     }
 
     /// @notice Returns the address of a deployment. Also handles the predeploys.
@@ -39,46 +59,7 @@ contract Artifacts {
         if (existing != address(0)) {
             return existing;
         }
-
-        bytes32 digest = keccak256(bytes(_name));
-        if (digest == keccak256(bytes("L2CrossDomainMessenger"))) {
-            return payable(Predeploys.L2_CROSS_DOMAIN_MESSENGER);
-        } else if (digest == keccak256(bytes("L2ToL1MessagePasser"))) {
-            return payable(Predeploys.L2_TO_L1_MESSAGE_PASSER);
-        } else if (digest == keccak256(bytes("L2StandardBridge"))) {
-            return payable(Predeploys.L2_STANDARD_BRIDGE);
-        } else if (digest == keccak256(bytes("L2ERC721Bridge"))) {
-            return payable(Predeploys.L2_ERC721_BRIDGE);
-        } else if (digest == keccak256(bytes("SequencerFeeWallet"))) {
-            return payable(Predeploys.SEQUENCER_FEE_WALLET);
-        } else if (digest == keccak256(bytes("OptimismMintableERC20Factory"))) {
-            return payable(Predeploys.OPTIMISM_MINTABLE_ERC20_FACTORY);
-        } else if (digest == keccak256(bytes("OptimismMintableERC721Factory"))) {
-            return payable(Predeploys.OPTIMISM_MINTABLE_ERC721_FACTORY);
-        } else if (digest == keccak256(bytes("L1Block"))) {
-            return payable(Predeploys.L1_BLOCK_ATTRIBUTES);
-        } else if (digest == keccak256(bytes("GasPriceOracle"))) {
-            return payable(Predeploys.GAS_PRICE_ORACLE);
-        } else if (digest == keccak256(bytes("L1MessageSender"))) {
-            return payable(Predeploys.L1_MESSAGE_SENDER);
-        } else if (digest == keccak256(bytes("WETH"))) {
-            return payable(Predeploys.WETH);
-        } else if (digest == keccak256(bytes("LegacyERC20ETH"))) {
-            return payable(Predeploys.LEGACY_ERC20_ETH);
-        } else if (digest == keccak256(bytes("ProxyAdmin"))) {
-            return payable(Predeploys.PROXY_ADMIN);
-        } else if (digest == keccak256(bytes("BaseFeeVault"))) {
-            return payable(Predeploys.BASE_FEE_VAULT);
-        } else if (digest == keccak256(bytes("L1FeeVault"))) {
-            return payable(Predeploys.L1_FEE_VAULT);
-        } else if (digest == keccak256(bytes("OperatorFeeVault"))) {
-            return payable(Predeploys.OPERATOR_FEE_VAULT);
-        } else if (digest == keccak256(bytes("SchemaRegistry"))) {
-            return payable(Predeploys.SCHEMA_REGISTRY);
-        } else if (digest == keccak256(bytes("EAS"))) {
-            return payable(Predeploys.EAS);
-        }
-        return payable(address(0));
+        return _predeploys[keccak256(bytes(_name))];
     }
 
     /// @notice Returns the address of a deployment and reverts if the deployment
