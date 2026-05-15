0f81dd30e15ba51b03c1f375224a03073fb03bc2
Jack Chuma <jack.chuma@coinbase.com>
2026-05-15 17:45:37 -0400

    clean CrossDomainMessenger.t.sol invariant

--- test/invariants/CrossDomainMessenger.t.sol
+++ test/invariants/CrossDomainMessenger.t.sol
@@ -13,12 +13,10 @@ import { Hashing } from "src/libraries/Hashing.sol";
 import { ForgeArtifacts } from "scripts/libraries/ForgeArtifacts.sol";
 
 contract RelayActor is StdUtils {
-    // Storage slot of the l2Sender
-    uint256 senderSlotIndex;
+    address internal constant IDENTITY_PRECOMPILE = address(0x04);
 
-    uint256 public numHashes;
     bytes32[] public hashes;
-    bool public reverted = false;
+    bool public reverted;
 
     IOptimismPortal2 op;
     IL1CrossDomainMessenger xdm;
@@ -30,30 +28,29 @@ contract RelayActor is StdUtils {
         xdm = _xdm;
         vm = _vm;
         doFail = _doFail;
-        senderSlotIndex = ForgeArtifacts.getSlot("OptimismPortal2", "l2Sender").slot;
+
+        // Set op.l2Sender() once to the L2 Cross Domain Messenger. Nothing in the fuzzed
+        // surface modifies this slot, so we don't need to re-write it on every relay.
+        uint256 senderSlotIndex = ForgeArtifacts.getSlot("OptimismPortal2", "l2Sender").slot;
+        vm.store(
+            address(_op), bytes32(senderSlotIndex), bytes32(uint256(uint160(Predeploys.L2_CROSS_DOMAIN_MESSENGER)))
+        );
+    }
+
+    function hashesLength() external view returns (uint256) {
+        return hashes.length;
     }
 
     /// @notice Relays a message to the `L1CrossDomainMessenger` with a random `version`,
     ///         and `_message`.
     function relay(uint8 _version, uint8 _value, bytes memory _message) external {
-        address target = address(0x04); // ID precompile
-        address sender = Predeploys.L2_CROSS_DOMAIN_MESSENGER;
-
-        // Set the minimum gas limit to the cost of the identity precompile's execution for
-        // the given message.
-        // ID Precompile cost can be determined by calculating: 15 + 3 * data_word_length
-        uint32 minGasLimit = uint32(15 + 3 * ((_message.length + 31) / 32));
-
-        // set the value of op.l2Sender() to be the L2 Cross Domain Messenger.
-        vm.store(address(op), bytes32(senderSlotIndex), bytes32(abi.encode(sender)));
-
-        // Restrict version to the range of [0, 1]
+        // Restrict version to [0, 1] and value to [0, 1] (variance with/without value;
+        // the ID precompile accepts value).
         _version = _version % 2;
-
-        // Restrict the value to the range of [0, 1]
-        // This is just so we get variance of calls with and without value. The ID precompile
-        // will not reject value being sent to it.
         _value = _value % 2;
+
+        // ID precompile gas cost: 15 + 3 * data_word_length.
+        uint32 minGasLimit = uint32(15 + 3 * ((_message.length + 31) / 32));
 
         // For the failure case, we use an impossibly large minGasLimit so that the hasMinGas
         // check always fails regardless of available gas. We provide baseGas-level gas (enough
@@ -64,31 +61,27 @@ contract RelayActor is StdUtils {
         uint32 relayMinGasLimit = doFail ? type(uint32).max : minGasLimit;
         uint256 gas = xdm.baseGas(_message, minGasLimit);
 
-        // Compute the cross domain message hash and store it in `hashes`.
-        // The `relayMessage` function will always encode the message as a version 1
-        // message after checking that the V0 hash has not already been relayed.
-        bytes32 _hash = Hashing.hashCrossDomainMessageV1(
-            Encoding.encodeVersionedNonce(0, _version), sender, target, _value, relayMinGasLimit, _message
-        );
+        // `relayMessage` always re-encodes as a v1 hash after checking the v0 hash hasn't been
+        // relayed, so the v1 hash is what we track.
+        uint256 nonce = Encoding.encodeVersionedNonce(0, _version);
+        address sender = Predeploys.L2_CROSS_DOMAIN_MESSENGER;
+        bytes32 _hash =
+            Hashing.hashCrossDomainMessageV1(nonce, sender, IDENTITY_PRECOMPILE, _value, relayMinGasLimit, _message);
         hashes.push(_hash);
-        numHashes += 1;
 
         // Make sure we've got a fresh message.
         vm.assume(xdm.successfulMessages(_hash) == false && xdm.failedMessages(_hash) == false);
 
-        // Act as the optimism portal and call `relayMessage` on the `L1CrossDomainMessenger` with
-        // the outer min gas limit.
         vm.startPrank(address(op));
         if (!doFail) {
-            vm.expectCallMinGas(address(0x04), _value, minGasLimit, _message);
+            vm.expectCallMinGas(IDENTITY_PRECOMPILE, _value, minGasLimit, _message);
         }
         try xdm.relayMessage{ gas: gas, value: _value }(
-            Encoding.encodeVersionedNonce(0, _version), sender, target, _value, relayMinGasLimit, _message
+            nonce, sender, IDENTITY_PRECOMPILE, _value, relayMinGasLimit, _message
         ) { }
         catch {
-            // If any of these calls revert, set `reverted` to true to fail the invariant test.
-            // NOTE: This is to get around forge's invariant fuzzer ignoring reverted calls
-            // to this function.
+            // Forge's invariant fuzzer ignores reverted target calls, so we surface the failure
+            // by flipping a flag the invariant asserts on.
             reverted = true;
         }
         vm.stopPrank();
@@ -99,38 +92,43 @@ contract XDM_MinGasLimits is CommonTest {
     RelayActor actor;
 
     function init(bool doFail) public virtual {
-        // Set up the `L1CrossDomainMessenger` and `OptimismPortal` contracts.
         super.setUp();
 
-        // Deploy a relay actor
         actor = new RelayActor(optimismPortal2, l1CrossDomainMessenger, vm, doFail);
 
-        // Give the portal some ether to send to `relayMessage`
+        // Give the portal some ether to send to `relayMessage`.
         vm.deal(address(optimismPortal2), type(uint128).max);
 
-        // Target the `RelayActor` contract
         targetContract(address(actor));
 
-        // Don't allow the estimation address to be the sender
         excludeSender(Constants.ESTIMATION_ADDRESS);
 
-        // Don't allow the predeploys to be the senders
+        // Don't allow the predeploys to be the senders.
         uint160 prefix = uint160(0x420) << 148;
-        for (uint256 i = 0; i < 2048; i++) {
-            address addr = address(prefix | uint160(i));
-            excludeContract(addr);
+        for (uint256 i = 0; i < Predeploys.PREDEPLOY_COUNT; i++) {
+            excludeContract(address(prefix | uint160(i)));
         }
 
-        // Target the actor's `relay` function
         bytes4[] memory selectors = new bytes4[](1);
         selectors[0] = actor.relay.selector;
         targetSelector(FuzzSelector({ addr: address(actor), selectors: selectors }));
+    }
+
+    /// @dev Asserts every relayed hash landed in the expected mapping. `expectSuccess = true`
+    ///      checks `successfulMessages`; `false` checks `failedMessages`.
+    function _assertHashes(bool expectSuccess) internal view {
+        uint256 length = actor.hashesLength();
+        for (uint256 i = 0; i < length; ++i) {
+            bytes32 hash = actor.hashes(i);
+            assertEq(l1CrossDomainMessenger.successfulMessages(hash), expectSuccess);
+            assertEq(l1CrossDomainMessenger.failedMessages(hash), !expectSuccess);
+        }
+        assertFalse(actor.reverted());
     }
 }
 
 contract XDM_MinGasLimits_Succeeds is XDM_MinGasLimits {
     function setUp() public override {
-        // Don't fail
         super.init(false);
     }
 
@@ -148,21 +146,12 @@ contract XDM_MinGasLimits_Succeeds is XDM_MinGasLimits {
     ///                   - The inner min gas limit is for the call from the
     ///                     `L1CrossDomainMessenger` to the target contract.
     function invariant_minGasLimits() external view {
-        uint256 length = actor.numHashes();
-        for (uint256 i = 0; i < length; ++i) {
-            bytes32 hash = actor.hashes(i);
-            // The message hash is set in the successfulMessages mapping
-            assertTrue(l1CrossDomainMessenger.successfulMessages(hash));
-            // The message hash is not set in the failedMessages mapping
-            assertFalse(l1CrossDomainMessenger.failedMessages(hash));
-        }
-        assertFalse(actor.reverted());
+        _assertHashes(true);
     }
 }
 
 contract XDM_MinGasLimits_Reverts is XDM_MinGasLimits {
     function setUp() public override {
-        // Do fail
         super.init(true);
     }
 
@@ -181,14 +170,6 @@ contract XDM_MinGasLimits_Reverts is XDM_MinGasLimits {
     ///                   - The inner min gas limit is for the call from the
     ///                     `L1CrossDomainMessenger` to the target contract.
     function invariant_minGasLimits() external view {
-        uint256 length = actor.numHashes();
-        for (uint256 i = 0; i < length; ++i) {
-            bytes32 hash = actor.hashes(i);
-            // The message hash is not set in the successfulMessages mapping
-            assertFalse(l1CrossDomainMessenger.successfulMessages(hash));
-            // The message hash is set in the failedMessages mapping
-            assertTrue(l1CrossDomainMessenger.failedMessages(hash));
-        }
-        assertFalse(actor.reverted());
+        _assertHashes(false);
     }
 }
