1d540c1227fa7515a599f377e189c08e13c07b1c
Jack Chuma <jack.chuma@coinbase.com>
2026-05-15 20:12:54 -0400

    Refactor SafeCall.t.sol: update SafeCaller_Actor constructor to accept harness address and failure expectation, rename performSafeCallMinGas to callWithMinGas for clarity, and improve gas handling logic in safe calls.

--- test/invariants/SafeCall.t.sol
+++ test/invariants/SafeCall.t.sol
@@ -7,9 +7,8 @@ import { SafeCall } from "src/libraries/SafeCall.sol";
 abstract contract SafeCall_Invariants is Test {
     SafeCaller_Actor actor;
 
-    function _init(bool _shouldFail) internal {
-        actor = new SafeCaller_Actor(_shouldFail);
-        targetSender(address(this));
+    function _init(bool _expectCallToFail) internal {
+        actor = new SafeCaller_Actor(address(this), _expectCallToFail);
         targetContract(address(actor));
 
         bytes4[] memory selectors = new bytes4[](1);
@@ -19,7 +18,7 @@ abstract contract SafeCall_Invariants is Test {
         vm.deal(address(actor), type(uint128).max);
     }
 
-    function performSafeCallMinGas(address to, uint64 minGas) external payable {
+    function callWithMinGas(address to, uint64 minGas) external payable {
         SafeCall.callWithMinGas(to, minGas, msg.value, hex"");
     }
 }
@@ -48,17 +47,18 @@ contract SafeCall_Fails_Invariants is SafeCall_Invariants {
 
 contract SafeCaller_Actor is Test {
     uint64 internal constant MIN_MIN_GAS = 2_500;
-    uint64 internal constant MAX_MIN_GAS = uint64(type(uint48).max);
-    uint64 internal constant CALL_WITH_MIN_GAS_OVERHEAD = 40_000;
-    uint64 internal constant SAFE_CALL_BUFFER = 1_000;
+    uint64 internal constant MAX_MIN_GAS = type(uint64).max / 64;
+    uint64 internal constant CALL_WITH_MIN_GAS_BUFFER = 40_000 + 1_000;
 
-    bool internal immutable shouldFail;
+    address internal immutable safeCallHarness;
+    bool internal immutable expectCallToFail;
 
     // Invariant handlers ignore target-call reverts, so failures must persist after the call.
     bool public badCallResult;
 
-    constructor(bool _shouldFail) {
-        shouldFail = _shouldFail;
+    constructor(address _safeCallHarness, bool _expectCallToFail) {
+        safeCallHarness = _safeCallHarness;
+        expectCallToFail = _expectCallToFail;
     }
 
     function performSafeCallMinGas(uint64 gas, uint64 minGas, address to, uint8 value) external {
@@ -66,18 +66,19 @@ contract SafeCaller_Actor is Test {
 
         minGas = uint64(bound(minGas, MIN_MIN_GAS, MAX_MIN_GAS));
         uint64 minCallGas = (minGas * 64) / 63;
-        if (shouldFail) {
+        if (expectCallToFail) {
             gas = uint64(bound(gas, minGas, minCallGas));
         } else {
-            gas = uint64(bound(gas, minCallGas + CALL_WITH_MIN_GAS_OVERHEAD + SAFE_CALL_BUFFER, type(uint64).max));
+            gas = uint64(bound(gas, minCallGas + CALL_WITH_MIN_GAS_BUFFER, type(uint64).max));
             vm.expectCallMinGas(to, value, minGas, hex"");
         }
 
         bool success = SafeCall.call(
-            msg.sender, gas, value, abi.encodeCall(SafeCall_Invariants.performSafeCallMinGas, (to, minGas))
+            safeCallHarness, gas, value, abi.encodeCall(SafeCall_Invariants.callWithMinGas, (to, minGas))
         );
 
-        if (success == shouldFail) {
+        bool expectedSuccess = !expectCallToFail;
+        if (success != expectedSuccess) {
             badCallResult = true;
         }
     }
